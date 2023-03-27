4b67269705b78ab4ab1b7071a405d5a41116f857
VladimirZag <zagorodnyukvolod@gmail.com>
2023-03-27 20:27:18 +0300

    added frame

--- src/MainCalculator.java
+++ src/MainCalculator.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class MainCalculator {
     public static void main(String[] args) {
-        
+        JFrame calculator = new JFrame("Калькулятор");
+        calculator.setSize(250, 350);
+        calculator.setResizable(false);
+        calculator.setLocationRelativeTo(null);
+        calculator.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        calculator.setVisible(true);
     }
 }
